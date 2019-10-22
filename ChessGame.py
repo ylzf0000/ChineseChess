8d30e4f64aa42430c9609ca2e642d05d89eb4d4a
Zhang <84978695@qq.com>
2019-10-22 16:03:48 +0800

    。

--- ChessGame.py
+++ ChessGame.py
@@ -14,14 +14,34 @@ class ChessGame:
     def start(self):
         self.view.start()
 
-    def callback(self, event):
-        print(event.x, event.y)
-        rx, ry = Global.coord_real2board(event.x), Global.coord_real2board(event.y)
-        if self.board.select(rx, ry, self.player_is_red):
+    def aigo(self):
+        for p in self.board.pieces:
+            pp = self.board.pieces[p]
+            if not pp.is_red:
+                moves = pp.get_move_locs(self.board)
+                if moves[0]:
+                    self.drop(pp.x, pp.y)
+                    self.drop(moves[0][0], moves[0][1])
+                    return
+
+    def drop(self, rx, ry):
+        ok = self.board.select(rx, ry, self.player_is_red)
+        if ok:
             self.player_is_red = not self.player_is_red
             self.view.showMsg("Red" if self.player_is_red else "Green")
         self.view.draw_board(self.board)
+        return ok
+
+    def callback(self, event):
+        print(event.x, event.y)
+        rx, ry = Global.coord_real2board(event.x), Global.coord_real2board(event.y)
+        if self.drop(rx, ry):
+            self.aigo()
+
 
 
 game = ChessGame()
 game.start()
+# while True:
+#     game.aigo()
+#     game.aigo()
