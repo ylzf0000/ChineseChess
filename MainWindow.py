c501b37e4e98876ff1a72b2204325202278b43c8
Zhang <84978695@qq.com>
2019-11-02 22:10:29 +0800

    使用pyqt5创建图形化界面

--- MainWindow.py
+++ MainWindow.py
@@ -4,6 +4,14 @@ from PyQt5.QtWidgets import QApplication, QMainWindow
 
 
 class MainWindow(QMainWindow, Ui_MainWindow):
+    squareSize = 73
+    boardEdge = 20
+    boardWidth = boardEdge + squareSize * 9 + boardEdge
+    boardHeight = boardEdge + squareSize * 10 + boardEdge
+
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
+        jpg = QtGui.QPixmap('images\IMAGES_X\WOOD.JPG')
+        self.labelImg.resize(jpg.width(), jpg.height())
+        self.labelImg.setPixmap(jpg)
--- main.py
+++ main.py
@@ -7,20 +7,6 @@ from MainWindow import *
 if __name__ == '__main__':
     mode = 'd'
     init_DLL(mode)
-    # Test
-    # start()
-    # aiMove()
-    # getSquares()
-    # print(getPlayer())
-    # print(getSquares())
-    # print(getValRed())
-    # print(getValBlack())
-    # print(getNStep())
-    # print(getSqSelected())
-    # print(getMvLast())
-    # print(isFlipped())
-    # End Test
-
     app = QApplication(sys.argv)
     mainWindow = MainWindow()
     mainWindow.show()
