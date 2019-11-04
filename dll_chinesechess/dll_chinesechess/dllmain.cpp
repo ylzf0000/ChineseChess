6014f903607f4226c1555c474403ab541381779f
Zhang <84978695@qq.com>
2019-11-04 23:07:22 +0800

    .

--- dll_chinesechess/dll_chinesechess/dllmain.cpp
+++ dll_chinesechess/dll_chinesechess/dllmain.cpp
@@ -1,66 +1,68 @@
 ﻿// dllmain.cpp : 定义 DLL 应用程序的入口点。
 #include "pch.h"
 #include "Alg.h"
+#include "utility.h"
+
 #define DLL_EXPORT __declspec(dllexport)
 Board& board = Board::Instance();
-extern "C"
+
+BEGIN_EXTERNC
+DLL_EXPORT void start()
 {
-	DLL_EXPORT void start()
-	{
-		board.Init();
-	}
-	DLL_EXPORT void aiMove()
-	{
-		responseMove();
-	}
-	DLL_EXPORT int playerMove(int src, int dst)
-	{
-		return board.PlayerMove_Checked(getMove(src, dst));
-	}
-	DLL_EXPORT int getPlayer()
-	{
-		return board.player;
-	}
-	DLL_EXPORT BYTE* getSquares()
-	{
-		return board.squares;
-	}
-	DLL_EXPORT int getValRed()
-	{
-		return board.valRed;
-	}
-	DLL_EXPORT int getValBlack()
-	{
-		return board.valBlack;
-	}
-	DLL_EXPORT int getNStep()
-	{
-		return board.nStep;
-	}
-	DLL_EXPORT int getSqSelected()
-	{
-		return board.sqSelected;
-	}
-	DLL_EXPORT int setSqselected(int pos)
-	{
-		int ret = isSelfChess(board.player, board.squares[pos]);
-		if (ret)
-			board.sqSelected = pos;
-		return ret;
-	}
-	DLL_EXPORT int getMvLast()
-	{
-		return board.mvLast;
-	}
-	DLL_EXPORT int isFlipped()
-	{
-		return board.isFlipped;
-	}
-	DLL_EXPORT BOOL isMate()
-	{
-		return board.IsMate();
-	}
+	board.Init();
 }
+DLL_EXPORT void aiMove()
+{
+	responseMove();
+}
+DLL_EXPORT int playerMove(int src, int dst)
+{
+	return board.PlayerMove_Checked(getMove(src, dst));
+}
+DLL_EXPORT int getPlayer()
+{
+	return board.player;
+}
+DLL_EXPORT BYTE* getSquares()
+{
+	return board.squares;
+}
+DLL_EXPORT int getValRed()
+{
+	return board.valRed;
+}
+DLL_EXPORT int getValBlack()
+{
+	return board.valBlack;
+}
+DLL_EXPORT int getNStep()
+{
+	return board.nStep;
+}
+DLL_EXPORT int getSqSelected()
+{
+	return board.sqSelected;
+}
+DLL_EXPORT int setSqselected(int pos)
+{
+	int ret = isSelfChess(board.player, board.squares[pos]);
+	if (ret)
+		board.sqSelected = pos;
+	return ret;
+}
+DLL_EXPORT int getMvLast()
+{
+	return board.mvLast;
+}
+DLL_EXPORT int isFlipped()
+{
+	return board.isFlipped;
+}
+DLL_EXPORT BOOL isMate()
+{
+	return board.IsMate();
+}
+END_EXTERNC
 
 
 BOOL APIENTRY DllMain(HMODULE hModule,
